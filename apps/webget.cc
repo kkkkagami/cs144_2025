e50c0257f5d0ed3a92d02011633d80ce84ee0e90
h_kagami <a17314336@163.com>
2025-07-25 18:16:54 +0800

    check0 3.5完成

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,8 +9,28 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  // cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
+  // cerr << "Warning: get_URL() has not been implemented yet.\n";
+
+  //根据域名host和服务名http创建addr对象
+  Address addr(host,"http");
+
+  TCPSocket tcp;//创建tcp套接字等待连接
+  tcp.connect(addr);//发起到服务器的连接
+
+  //write方法，将buffer写入套接字，向已建立的tcp发送数据
+  tcp.write("GET "+path+" HTTP/1.1\r\n");
+  tcp.write("Host:"+host+"\r\n");
+  tcp.write("Connection:close\r\n");
+  tcp.write("\r\n");
+
+  //套接字会向应用层返回数据，通过循环读取到buffer并输出
+  while(!tcp.eof()){
+    std::string buffer;
+    tcp.read(buffer);
+    cout<<buffer;
+  }
+  tcp.close();
 }
 
 int main( int argc, char* argv[] )
