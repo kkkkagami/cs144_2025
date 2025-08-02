6c560e2504dd3722ba3f33efb406a01f6ba0483d
h_kagami <a17314336@163.com>
2025-08-02 11:18:09 +0800

    check1 准备修改的fd

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -9,18 +9,63 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   
   Writer& writeable_output=output_.writer();//将output_转换成可写的Writer&类型
 
-  //step0.确认is_last_substring的情况
+  //step0.确认is_last_substring的情况----------------------------------------------------------------
   if(is_last_substring){
     //如果获取了最后一部分输入的字节,更新final_byte_index_确认末位索引
     final_byte_index_=first_index+data.size();
     have_last_substring_received_=true;
   }
 
-  if (have_last_substring_received_ && data.empty() && first_index == next_expected_index_) {
-    writeable_output.close();
-    return;
+  // if (have_last_substring_received_ && data.empty() && first_index == next_expected_index_) {
+  //   writeable_output.close();
+  //   return;
+  // }
+
+
+  
+
+  //step2.根据当前收到的data的首位索引first_index和长度len_data确定当前收到数据的索引范围-------------------
+  //并根据范围情况预处理data
+  uint64_t last_index=first_index+data.size();//当前收到的data的最后一位的索引
+
+  //2.1 last_index超出了'最大可接受索引' -->直接截断多余部分
+  //最大可接受索引[这个索引位置是最大的未output_范围的索引]
+  uint64_t max_acceptable_index=writeable_output.bytes_pushed()+writeable_output.available_capacity();
+  if(last_index>max_acceptable_index) { 
+    if(first_index>max_acceptable_index){
+      //data首位已经超出可接受范围，直接返回
+      return;
+    }
+    last_index=max_acceptable_index;
+    data=data.substr(0,last_index-first_index);//data自后方截断 
   }
 
+  //2.2 first_index还没有达到'最小需要索引'
+  if(first_index<next_expected_index_) { 
+    if(last_index<next_expected_index_) { 
+      //data的末位也比next_expected_index_小，那么这段数据已经传送过，函数返回
+      return; 
+    }
+    uint64_t overlap_len=next_expected_index_-first_index;//截断长度
+    first_index=next_expected_index_;
+    data=data.substr(overlap_len);//data自前方截断
+  }
+
+  
+  //step3.first_index与next_expected_index_是什么样的关系？----------------------------------------------
+  //3.1 如果相等，那么进行下面的操作：直接将data送入output_(用writeable_output的形式)
+  if(first_index==next_expected_index_){
+    writeable_output.push(data);
+    next_expected_index_+=data.size();
+
+    if(next_expected_index_==final_byte_index_&&have_last_substring_received_==true){
+      writeable_output.close();
+    }
+  }
+  else
+  //3.2 如果不相等，那只能是first_index>next_expected_index_ [小于的情况在step1已经清除]
+  //那么需要将这段data放入map中暂存
+   stroed_segments_[first_index]=data;
 
   //step1.检查map中是否有可以送入output_的数据----------------------------------------------------------
   while(true){
@@ -54,7 +99,7 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
 
       stroed_segments_[segment_insert_first_index]=segment_insert_data;
 
-      break;
+      continue;
     }
 
     //1.2.2  不需要裁剪
@@ -69,51 +114,6 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
       writeable_output.close();
     }
   }
-
-  //step2.根据当前收到的data的首位索引first_index和长度len_data确定当前收到数据的索引范围-----------------
-  //并根据范围情况预处理data
-  uint64_t last_index=first_index+data.size()-1;//当前收到的data的最后一位的索引
-
-  //2.1 last_index超出了'最大可接受索引' -->直接截断多余部分
-  //最大可接受索引[这个索引位置是最大的未output_范围的索引]
-  uint64_t max_acceptable_index=writeable_output.bytes_pushed()+writeable_output.available_capacity();
-  if(last_index>max_acceptable_index) { 
-    if(first_index>max_acceptable_index){
-      //data首位已经超出可接受范围，直接返回
-      return;
-    }
-    last_index=max_acceptable_index;
-    data=data.substr(0,last_index-first_index+1);//data自后方截断 
-  }
-
-  //2.2 first_index还没有达到'最小需要索引'
-  if(first_index<next_expected_index_) { 
-    if(last_index<next_expected_index_) { 
-      //data的末位也比next_expected_index_小，那么这段数据已经传送过，函数返回
-      return; 
-    }
-    uint64_t overlap_len=next_expected_index_-first_index;//截断长度
-    first_index=next_expected_index_;
-    data=data.substr(overlap_len);//data自前方截断
-  }
-
-  
-  //step3.first_index与next_expected_index_是什么样的关系？----------------------------------------------
-  //3.1 如果相等，那么进行下面的操作：直接将data送入output_(用writeable_output的形式)
-  if(first_index==next_expected_index_){
-    writeable_output.push(data);
-
-    if(next_expected_index_==final_byte_index_&&have_last_substring_received_==true){
-      writeable_output.close();
-    }
-  }
-
-  //3.2 如果不相等，那只能是first_index>next_expected_index_ [小于的情况在step1已经清除]
-  //那么需要将这段data放入map中暂存
-  stroed_segments_[first_index]=data;
-
-  //step4.检查是否是最后一位传入的数据，是则关闭输入
-  //if(is_last_substring)  writeable_output.close();
 }
 
 // How many bytes are stored in the Reassembler itself?
